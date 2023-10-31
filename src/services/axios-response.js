1f1f06c5db8e795aed5e85d9c98ebcfb1ee7e8f9
DATAGNI K Armand Noël <datagni.k.armand.n@gmail.com>
2023-10-31 15:44:29 +0000

    ...

--- src/services/axios-response.js
+++ src/services/axios-response.js
@@ -1,13 +1,13 @@
-export const retriveAxiosSuccessResponse = (res, data) => {
+export const retriveAxiosSuccessResponse = (res) => {
     return {
         status: res.status,
         message: res.data.message,
         success: res.data.success,
-        data: data ? res.data.data[data] : res.data.data,
-        pages: data?.pages ? res.pages : 1,
-        total: data?.total ? res.total : 1,
-        page: data?.page ? res.page : 1,
-        per_page: data?.per_page ? res.per_page : 1,
+        data: res.data.data,
+        pages: res.data.pages,
+        total: res.data.total,
+        page: res.data.page,
+        per_page: res.data.per_page,
     }
 };
 
